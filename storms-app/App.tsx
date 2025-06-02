e9685f71c918a0472f1477abdea972909bc7ab62
gabimmdev <gabrielagomescezar@gmail.com>
2025-06-02 13:12:26 -0300

    fix: delete unused stack navigator

--- storms-app/App.tsx
+++ storms-app/App.tsx
@@ -3,10 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
-import AlertScreen from './screens/AlertScreen';
 import SensorDetailsScreen from './screens/SensorDetailsScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import { RootStackParamList } from './types/index';
-
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -15,9 +14,9 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="SensorDetails" component={SensorDetailsScreen} />
-        <Stack.Screen name="Alert" component={AlertScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
