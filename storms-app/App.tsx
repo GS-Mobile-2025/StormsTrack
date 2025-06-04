c65f635c14acda35031b9388f2a7c2935bc4ca45
gabimmdev <gabrielagomescezar@gmail.com>
2025-06-04 00:23:38 -0300

    chore: configuring stack navigator for main screens

--- storms-app/App.tsx
+++ storms-app/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
 import SensorDetailsScreen from './screens/SensorDetailsScreen';
-import RegisterScreen from './screens/RegisterScreen';
-import { RootStackParamList } from './types/index';
+import { RootStackParamList } from './types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -13,10 +13,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
         <Stack.Screen name="SensorDetails" component={SensorDetailsScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
