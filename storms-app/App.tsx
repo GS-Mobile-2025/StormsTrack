b5bccc87d880eccafcffc2c3a5ab31e86953d7bb
gabimmdev <gabrielagomescezar@gmail.com>
2025-06-07 00:04:48 -0300

    refactor: delete unused screen

--- storms-app/App.tsx
+++ storms-app/App.tsx
@@ -4,7 +4,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
-import SensorDetailsScreen from './screens/SensorDetailsScreen';
 import SensorFormScreen from './screens/SensorFormScreen';
 import AlertFormScreen from './screens/AlertFormScreen';
 import { RootStackParamList } from './types';
@@ -18,7 +17,6 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="SensorDetails" component={SensorDetailsScreen} />
         <Stack.Screen name="SensorForm" component={SensorFormScreen} />
         <Stack.Screen name="AlertForm" component={AlertFormScreen} />
       </Stack.Navigator>
